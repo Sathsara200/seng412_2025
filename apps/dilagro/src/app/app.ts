8a6e78e52a891aa490cecb3a2ddfe163cdd10827
Tharidu Sathsara <tharidusathsara200@gmail.com>
2025-09-05 12:04:14 +0530

    create login component😁

--- apps/dilagro/src/app/app.ts
+++ apps/dilagro/src/app/app.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Login } from './pages/login/login';
 
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, Login, CommonModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'dilagro';
+  name = "Sathsara";
+  frameworks = ['Angular', 'React', 'Vue'];
+
+  onClick() {
+    this.name = "Tharidu"
+  }
 }
