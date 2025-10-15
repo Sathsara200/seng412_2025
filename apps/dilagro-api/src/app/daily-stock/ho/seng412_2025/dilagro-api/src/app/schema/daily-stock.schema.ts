01674cbf0ef5a063e34a44c3ea617ea457c44b12
Sathsara200 <tharidusathsara200@gmail.com>
2025-10-15 17:56:09 +0530

    bug fix

--- apps/dilagro-api/src/app/daily-stock/ho/seng412_2025/dilagro-api/src/app/schema/daily-stock.schema.ts
+++ apps/dilagro-api/src/app/daily-stock/ho/seng412_2025/dilagro-api/src/app/schema/daily-stock.schema.ts
@@ -1,11 +1,15 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { IDailyStock } from '@SENG412_2025/model';
 import { HydratedDocument } from "mongoose";
+
+export interface IDailyStock {
+    date: Date;
+    amount: number;
+}
 
 @Schema()
 export class DailyStock implements IDailyStock {
     @Prop() date: Date;
-    @Prop() amount: number
+    @Prop() amount: number;
 }
 
 export type CatDocument = HydratedDocument<DailyStock>;
