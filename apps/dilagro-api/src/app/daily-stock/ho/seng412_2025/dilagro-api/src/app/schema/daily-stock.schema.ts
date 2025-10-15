00864c3d78e389c502a8bebc6539faedde5d47d2
Sathsara200 <tharidusathsara200@gmail.com>
2025-10-15 17:55:45 +0530

    bug fix

--- apps/dilagro-api/src/app/daily-stock/ho/seng412_2025/dilagro-api/src/app/schema/daily-stock.schema.ts
+++ apps/dilagro-api/src/app/daily-stock/ho/seng412_2025/dilagro-api/src/app/schema/daily-stock.schema.ts
@@ -3,7 +3,6 @@ import { IDailyStock } from '@SENG412_2025/model';
 import { HydratedDocument } from "mongoose";
 
 @Schema()
-
 export class DailyStock implements IDailyStock {
     @Prop() date: Date;
     @Prop() amount: number
